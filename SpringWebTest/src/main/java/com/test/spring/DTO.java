633bc2a7a7f342aaa6fcd40f875bedfb6c9bf009
sunday-sunny <22mallox@gmail.com>
2022-02-05 00:03:34 +0900

    Add 2022-02-04(금) 수업 파일 (lombok 라이브러리 추가 및 예제)

---
+++ SpringWebTest/src/main/java/com/test/spring/DTO.java
@@ -0,0 +1,17 @@
+package com.test.spring;
+
+import lombok.Data;
+
+// 아래의 어노테이션을 사용해서 getter, setter, toString을 자동으로 만들어주는 기능
+@Data
+public class DTO {
+	
+	private String seq;
+	private String name;
+	private String id;
+	private String subject;
+	private String content;
+	private String regdate;
+
+
+}
---
+++ SpringWebTest/src/main/java/com/test/spring/Ex01.java
@@ -0,0 +1,16 @@
+package com.test.spring;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+
+		DTO dto = new DTO();
+		
+		dto.setSeq("hong");
+		dto.setName("홍길동");
+		
+		System.out.println(dto.getName());
+		
+	}
+
+}
